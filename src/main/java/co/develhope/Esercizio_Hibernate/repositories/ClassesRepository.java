ea30b593e10a8594a6d889b18579cd5e4783625a
Felipe Arosio <felipe_arosio@outlook.com>
2024-06-04 20:49:05 +0200

    Repositories

---
+++ src/main/java/co/develhope/Esercizio_Hibernate/repositories/ClassesRepository.java
@@ -0,0 +1,8 @@
+package co.develhope.Esercizio_Hibernate.repositories;
+
+import co.develhope.Esercizio_Hibernate.entities.Classes;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClassesRepository extends JpaRepository<Classes, Long> {
+}
+
